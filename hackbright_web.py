fd5afa10e26b64aadf35962abb4265f9d8ff5b7c
Bhawna Mulchandani <bhawna.bm@gmail.com>
2021-05-20 23:38:57 +0000

    Update /students route to use jinja varibles, created student_info file to be rendered for the get_student route

--- hackbright_web.py
+++ hackbright_web.py
@@ -11,11 +11,12 @@ app = Flask(__name__)
 def get_student():
     """Show information about a student."""
 
-    github = "jhacks"
+    github = request.args.get('github')
 
     first, last, github = hackbright.get_student_by_github(github)
 
-    return "{} is the GitHub account for {} {}".format(github, first, last)
+    # return f"{github} is the GitHub account for {first} {last}"
+    return render_template('student_info.html', first_name=first, last_name=last, github_account=github)
 
 if __name__ == "__main__":
     hackbright.connect_to_db(app)
