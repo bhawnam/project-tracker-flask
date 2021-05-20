f31ac4a69e657799ab3a755770b654ce75161dbd
Bhawna Mulchandani <bhawna.bm@gmail.com>
2021-05-20 23:53:18 +0000

    Created a student_search.html for having a form for user to enter gihub and made a route /student-search to render that form

--- hackbright_web.py
+++ hackbright_web.py
@@ -18,6 +18,14 @@ def get_student():
     # return f"{github} is the GitHub account for {first} {last}"
     return render_template('student_info.html', first_name=first, last_name=last, github_account=github)
 
+
+@app.route("/student-search")
+def get_student_form():
+    """Show a form for searching a student. """
+
+    return render_template('student_search.html')
+
+
 if __name__ == "__main__":
     hackbright.connect_to_db(app)
     app.run(debug=True, host="0.0.0.0")
