34a9103d9b71b52e6cd909b11dbed1d8f3d63989
Bhawna Mulchandani <bhawna.bm@gmail.com>
2021-05-21 22:24:21 +0000

    Added student_add_form and student_added html files to get info to add a student and diaply that the student was added. Also, create routes to render these files

--- hackbright_web.py
+++ hackbright_web.py
@@ -26,6 +26,25 @@ def get_student_form():
     return render_template('student_search.html')
 
 
+@app.route("/student-add", methods=['POST'])
+def student_add():
+    """ Add a student."""
+
+    first = request.form.get('first')
+    last = request.form.get('last')
+    github = request.form.get('github')
+
+    hackbright.make_new_student(first, last, github)
+
+    return render_template('student_added.html', github_account = github)
+
+
+@app.route("/student-add-form")
+def student_add_form():
+    """ Show a form to add a new student. """    
+
+    return render_template("student_add_form.html")
+
 if __name__ == "__main__":
     hackbright.connect_to_db(app)
     app.run(debug=True, host="0.0.0.0")
